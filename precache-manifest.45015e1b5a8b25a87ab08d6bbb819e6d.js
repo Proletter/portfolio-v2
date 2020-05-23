e67e85d5fe68cf6827f4fe6bf877290c7c43213c
proletter <oluseun.ismaila@gmail.com>
2020-05-23 11:48:57 +0100

    Updates

---
+++ precache-manifest.45015e1b5a8b25a87ab08d6bbb819e6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8ecfc708599f25c0195a7e5f5f9bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7419d1bc3508eb4559da",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6b5e8f6ddbf11281273e",
+    "url": "/static/css/main.4f40f877.chunk.css"
+  },
+  {
+    "revision": "7419d1bc3508eb4559da",
+    "url": "/static/js/2.196c9298.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.196c9298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5e8f6ddbf11281273e",
+    "url": "/static/js/main.91a7d427.chunk.js"
+  },
+  {
+    "revision": "f642ae2f812f09af5543",
+    "url": "/static/js/runtime-main.fc71e450.js"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/static/media/pic.46d9f22b.svg"
+  }
+]);
