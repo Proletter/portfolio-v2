211475f31f7ff4513e1a82cfe5346677ff50c3d0
proletter <oluseun.ismaila@gmail.com>
2020-05-23 13:40:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ec75b53cd068e6e00172f1bba748e43.js"
+  "/precache-manifest.fba0a7659fddbff019e9b8d661125ac2.js"
 );
 
 self.addEventListener('message', (event) => {
